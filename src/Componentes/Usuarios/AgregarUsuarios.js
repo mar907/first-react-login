6888934508de7d918d78965e6aa62235948dd29d
Veronica Fernandez <mariaveronicafernandez4@gmail.com>
2021-08-06 10:24:29 -0300

    cambios en el agregar usuarios

--- src/Componentes/Usuarios/AgregarUsuarios.js
+++ src/Componentes/Usuarios/AgregarUsuarios.js
@@ -9,39 +9,39 @@ import ErrorModal from "../Usuarios/ErrorModal";
 const AgregarUsuarios = (props) => {
     const [enteredEmail, setEnteredEmail] = useState('');
     const [enteredPassword, setEnteredPassword] = useState('');
-    const [error, setError] =useState ('');
+    const [error, setError] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
-        if (enteredEmail.trim().length === 0 || enteredPassword.trim().length === 0) {
-            setError ({
-                title:'Email invalido',
-                message:'Por favor ingrese un email y contraseña validos. (no empty values)'
+        if (enteredEmail.trim().length === 0) {
+            setError({
+                title: 'Email invalido',
+                message: 'Por favor ingrese un email y contraseña validos. (no empty values)'
             });
             return;
         }
 
         if (enteredPassword.trim().length === 0) {
-            setError ({
-                title:'Contraseña invalida',
-                message:'Por favor ingrese una contraseña valida. (no empty values)'
+            setError({
+                title: 'Contraseña invalida',
+                message: 'Por favor ingrese una contraseña valida. (no empty values)'
             });
             return;
-        } 
+        }
 
-        if(enteredEmail.trim().length !== 0 || enteredPassword.trim().length !==
-         0) {
-            setError ({
+        if (enteredEmail.trim().length !== 0 || enteredPassword.trim().length !==
+            0) {
+            setError({
                 title: 'Ambos datos son correctos',
-                message: 'Hola $`{enteredEmail}` bienvenido'
+                message: `Hola ${enteredEmail} bienvenido`
 
             });
             return;
         }
 
         props.onAddUser(enteredEmail, enteredPassword);
-        setEnteredEmail ('');
-        setEnteredPassword ('');
+        setEnteredEmail('');
+        setEnteredPassword('');
     };
 
     const emailChangeHandler = (event) => {
@@ -51,34 +51,34 @@ const AgregarUsuarios = (props) => {
     const passwordChangeHandler = (event) => {
         setEnteredPassword(event.target.value);
     };
-    const errorHandler= () =>{
-        setError (null);
+    const errorHandler = () => {
+        setError(null);
     };
 
     return (
         <div>
-        {error && (
-        <ErrorModal 
-        title= {error.title} 
-        message= {error.message} 
-        onConfirm ={errorHandler}
-        />
-        )}
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor='email'>Email</label>
-                <input id='email'
-                    type='email'
-                    value={enteredEmail}
-                    onChange={emailChangeHandler} />
-                <label htmlFor='password'>Contraseña</label>
-                <input id='contraseña'
-                    type='password'
-                    value={enteredPassword}
-                    onChange={passwordChangeHandler} />
-                <Button type='submit'> Agregar Usuario</Button>
-            </form>
-        </Card>
+            {error && (
+                <ErrorModal
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor='email'>Email</label>
+                    <input id='email'
+                        type='email'
+                        value={enteredEmail}
+                        onChange={emailChangeHandler} />
+                    <label htmlFor='password'>Contraseña</label>
+                    <input id='contraseña'
+                        type='password'
+                        value={enteredPassword}
+                        onChange={passwordChangeHandler} />
+                    <Button type='submit'> Agregar Usuario</Button>
+                </form>
+            </Card>
         </div>
     );
 };
